095f4bc81985ea1b6c5a0d90c7bd717291fe8e0f
Farinda <enjokeek@gmail.com>
2018-06-26 11:28:05 +0700

    Tampilan awal dan config
    Tampilan awal dan config

---
+++ application/views/home/home.php
@@ -0,0 +1,56 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../../../../favicon.ico">
+
+  <title>Laundry</title>
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="<?php echo base_url('vendor/bootstrap/css/bootstrap.min.css') ?>">
+
+  <!-- Custom styles for this template -->
+  <link href="<?php echo base_url() ?>assets/css/cover.css" rel="stylesheet">
+</head>
+
+<body class="text-center" background="<?php echo base_url('assets/img/bg.jpg') ?>" style="background-size: cover;">
+
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+      <div class="inner">
+        <h3 class="masthead-brand">Laundry</h3>
+        <nav class="nav nav-masthead justify-content-center">
+          <a class="nav-link active" href="#">Home</a>
+          
+        </nav>
+      </div>
+    </header>
+
+    <main role="main" class="inner cover">
+      <h1 class="cover-heading">Wash Clean Dry</h1>
+      <p class="lead">Laundry untuk pakaianmu yang kotor</p>
+      <p class="lead">
+        <a href="#" class="btn btn-lg btn-secondary">Visi & Misi</a>
+      </p>
+    </main>
+
+    <footer class="mastfoot mt-auto">
+      <div class="inner">
+        <p>copyright &copy; 2018</p>
+      </div>
+    </footer>
+  </div>
+
+
+    <!-- Bootstrap core JavaScript
+      ================================================== -->
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script src="<?php echo base_url('vendor/jquery/js/jquery.min.js') ?>"></script>
+      <script src="<?php echo base_url('vendor/popper/js/popper.min.js') ?>"></script>
+      <script src="<?php echo base_url('vendor/bootstrap/js/bootstrap.min.js') ?>"></script>
+    </body>
+    </html>
