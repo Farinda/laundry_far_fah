889d10b18e687fc6bc9a3869d0f13070d6848723
Farinda <enjokeek@gmail.com>
2018-06-26 11:51:27 +0700

    header footer
    memisahkan home.php menjadi header footer

---
+++ application/views/home/footer.php
@@ -0,0 +1,16 @@
+    <footer class="mastfoot mt-auto">
+      <div class="inner">
+        <p>copyright &copy; 2018</p>
+      </div>
+    </footer>
+  </div>
+
+
+    <!-- Bootstrap core JavaScript
+      ================================================== -->
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script src="<?php echo base_url('vendor/jquery/js/jquery.min.js') ?>"></script>
+      <script src="<?php echo base_url('vendor/popper/js/popper.min.js') ?>"></script>
+      <script src="<?php echo base_url('vendor/bootstrap/js/bootstrap.min.js') ?>"></script>
+    </body>
+    </html>
---
+++ application/views/home/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../../../../favicon.ico">
+
+  <title>Laundry</title>
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="<?php echo base_url('vendor/bootstrap/css/bootstrap.min.css') ?>">
+
+  <!-- Custom styles for this template -->
+  <link href="<?php echo base_url() ?>assets/css/cover.css" rel="stylesheet">
+</head>
+
+<body class="text-center" background="<?php echo base_url('assets/img/bg.jpg') ?>" style="background-size: cover;">
+
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+      <div class="inner">
+        <h3 class="masthead-brand">Laundry</h3>
+        <nav class="nav nav-masthead justify-content-center">
+          <a class="nav-link active" href="#">Home</a>
+          
+        </nav>
+      </div>
+    </header>
